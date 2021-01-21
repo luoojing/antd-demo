96ba022288ab3e8eceb585b7a8d472f274e1031d
luoojing <lisa_coder@outlook.com>
2021-01-21 13:51:30 +0800

    button & icon

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+
+import {Button } from 'ant-design-vue'
+import { Icon } from 'ant-design-vue';
 import App from './App.vue'
 
+
+Vue.component(Icon.name,Icon)
+Vue.component(Button.name,Button)
+
+
 Vue.config.productionTip = false
+
+
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
